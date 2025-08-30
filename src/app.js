5be0636feace1e04521941c18792502d189b83b9
Muralikrishnakoduru462 <muralikrishnakoduru462@gmail>
2025-08-30 22:27:30 +0530

    handling multiple routes

--- src/app.js
+++ src/app.js
@@ -2,28 +2,21 @@ const express = require("express");
 
 const app = express();
 
-app.use("/user", [
-  (req, res, next) => {
-    console.log("User route is called");
-    // res.send("Hello World");
-    next();
-  },
-  (req, res, next) => {
-    console.log("User route is called2");
-    // res.send("Hello World2");
-    next();
-  },
-  (req, res, next) => {
-    console.log("User route is called3");
-    // res.send("Hello World3");
-    next();
-  },
-  (req, res, next) => {
-    console.log("User route is called4");
-    res.send("Hello World4");
-    // next();
-  },
-]);
+const { adminAuth, userAuth } = require("./middlewares/auth");
+
+app.use("/admin", adminAuth);
+
+app.get("/user", userAuth, (req, res) => {
+  res.send("Welcome to Home Page");
+});
+
+app.get("/admin/getAllData", (req, res) => {
+  res.send("All Data Sent Successfully");
+});
+
+app.delete("/admin/deleteUser", (req, res) => {
+  res.send("Delete User Successfully");
+});
 
 app.listen(7777, () => {
   console.log("Server is successfully running");
