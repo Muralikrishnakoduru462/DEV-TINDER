194b4b4f5d5f6ac26e1bc7eebe73bf521e673f9e
Muralikrishnakoduru462 <muralikrishnakoduru462@gmail>
2025-08-30 19:20:05 +0530

    exploring http methods

--- src/app.js
+++ src/app.js
@@ -2,16 +2,24 @@ const express = require("express");
 
 const app = express();
 
-app.use("/", (req, res) => {
-  res.send("//// for universal");
+app.use("/user", (req, res) => {
+  res.send("use user");
 });
 
-app.use("/hello", (req, res) => {
-  res.send("hello hello hello");
+app.get("/user", (re, res) => {
+  res.send({ firstName: "murali", lastName: "krishna" });
+});
+
+app.post("/user", (re, res) => {
+  res.send("post call user");
+});
+
+app.delete("/user", (re, res) => {
+  res.send("post call user");
 });
 
 app.use("/test", (req, res) => {
-  res.send("test test test");
+  res.send("delete call");
 });
 
 app.listen(7777, () => {
