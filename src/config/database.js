56ac14794da174a9a180704b55bed34dcba76b3b
Muralikrishnakoduru462 <muralikrishnakoduru462@gmail>
2025-09-16 21:08:55 +0530

    envIntegration

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  await mongoose.connect(
-    "mongodb+srv://namastedev:sviHkLP8Plkse6E1@namastenode.enqaszj.mongodb.net/devTinder"
-  );
+  await mongoose.connect(process.env.DB_CONNECTION_SECRET);
 };
 
 module.exports = connectDB;
